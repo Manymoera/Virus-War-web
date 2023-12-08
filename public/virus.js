4447a85804fe92f7583607feb4ede75714dbc050
Manymoera <soloninko80@gmail.com>
2023-12-08 21:28:17 +0300

    started making server side, connected socket.io

--- public/virus.js
+++ public/virus.js
@@ -6,7 +6,6 @@
   21 = red circle
   3 - borders
 */
-
 /*
   Варианты победы:
   1. Крестики заняли последний нолик
@@ -15,18 +14,19 @@
   4. Нолики так распространились, что крестиками больше некуда идти
 */
 
+import { io } from "https://cdn.socket.io/4.7.2/socket.io.esm.min.js";
+
+const socket = io("http://localhost:3000");
+
 function Counter() {
   this.value = 0;
 }
-
 Counter.prototype.next = function () {
   return this.value++;
 };
-
 Counter.prototype.make_null = function () {
   return this.value = 0;
 };
-
 Counter.prototype.comp = function (val) {
   if (this.value == val) {
     return true;
@@ -36,17 +36,46 @@ Counter.prototype.comp = function (val) {
   }
 };
 
-function check_victory(tableArr, player, i, j) {
+function handle_turn_change() {
+  alert('made the move');
+}
+
+function check_victory(tableArr, player) {
   let av_count = 0;
-  for (let x = i - 1; x < i + 2; x++) {
-    for (let y = j - 1; y < j + 2; y++) {
-      if (tableArr[x][y] == 0 || tableArr[x][y] == 2) {
-        av_count++;
+  let cir_count = 0;
+  let cross_count = 0;
+  for (let i = 1; i < tableArr.length - 1; i++) {
+    for (let j = 1; j < tableArr[i].length - 1; j++) {
+      if ((player == 'X' && tableArr[i][j] == 1) ||
+        (player == 'O' && tableArr[i][j] == 2)) {
+        for (let x = i - 1; x < i + 2; x++) {
+          for (let y = j - 1; y < j + 2; y++) {
+            if ((player == 'X' && (tableArr[x][y] == 0 || tableArr[x][y] == 2 || tableArr[x][y] == 21)) ||
+              (player == 'O' && (tableArr[x][y] == 0 || tableArr[x][y] == 1 || tableArr[x][y] == 12))) {
+              av_count++;
+            }
+          }
+        }
+      }
+      if (tableArr[i][j] == 2) {
+        cir_count++;
+      }
+      if (tableArr[i][j] == 1) {
+        cross_count++;
       }
     }
   }
   if (av_count == 0) {
-    alert("win");
+    alert('win');
+    return;
+  }
+  else if (cir_count == 0) {
+    alert('crosses win');
+    return;
+  }
+  else if (cross_count == 0) {
+    alert('circles win');
+    return;
   }
 }
 
@@ -58,11 +87,13 @@ function check_cell(tableArr, newCell, player, turn_count, i, j) {
           if (tableArr[i][j] == 0) {
             newCell.innerHTML = '<img src="assets/oax_CX.gif">';
             tableArr[i][j] = 1;
+            check_victory(tableArr, player, i, j);
             turn_count.next();
           }
           else if (tableArr[i][j] == 2) {
             newCell.innerHTML = '<img src="assets/oax_CP.gif">';
             tableArr[i][j] = 21;
+            check_victory(tableArr, player, i, j);
             turn_count.next();
           }
         }
@@ -76,11 +107,13 @@ function check_cell(tableArr, newCell, player, turn_count, i, j) {
           if (tableArr[i][j] == 0) {
             newCell.innerHTML = '<img src="assets/oax_CO.gif">';
             tableArr[i][j] = 2;
+            check_victory(tableArr, player, i, j);
             turn_count.next();
           }
           else if (tableArr[i][j] == 1) {
             newCell.innerHTML = '<img src="assets/oax_CY.gif">';
             tableArr[i][j] = 12;
+            check_victory(tableArr, player, i, j);
             turn_count.next();
           }
         }
@@ -88,17 +121,22 @@ function check_cell(tableArr, newCell, player, turn_count, i, j) {
     }
   }
   if (turn_count.comp(3)) {
-    alert('made the move');
+    handle_turn_change();
     turn_count.make_null();
   }
-  check_victory(tableArr, player, i, j);
 }
 
-function createTable(table, tableArr, parent, turn_count, player) {
+document.addEventListener("DOMContentLoaded", function () {
+  createTable(tableArr, turn_count, active_player);
+});
+
+function createTable(tableArr, turn_count, active_player) {
+  let table = document.getElementById('game_field');
+  let tbody = table.getElementsByTagName('tbody')[0];
   for (let i = 1; i < tableArr.length - 1; i++) {
-    let row = table.insertRow();
+    let row = document.createElement('tr');
     for (let j = 1; j < tableArr[i].length - 1; j++) {
-      let newCell = row.insertCell();
+      let newCell = document.createElement('td');
       if (tableArr[i][j] == 0) {
         newCell.innerHTML = '<img src="assets/oax_W.gif">';
       }
@@ -115,27 +153,25 @@ function createTable(table, tableArr, parent, turn_count, player) {
         newCell.innerHTML = '<img src="assets/oax_CP.gif">';
       }
       newCell.onclick = function () {
-        check_cell(tableArr, newCell, player, turn_count, i, j);
+        check_cell(tableArr, newCell, active_player, turn_count, i, j);
       };
+      row.appendChild(newCell);
     }
+    tbody.appendChild(row);
   }
-  parent.appendChild(table);
 }
 
 var parent_table = document.getElementById('table-container');
 var tableArr = [
   [3, 3, 3, 3, 3, 3, 3, 3],
-  [3, 0, 1, 1, 1, 1, 1, 3],
-  [3, 0, 1, 1, 1, 1, 1, 3],
-  [3, 0, 1, 1, 1, 1, 1, 3],
-  [3, 0, 1, 1, 1, 1, 1, 3],
-  [3, 0, 1, 1, 1, 1, 1, 3],
-  [3, 2, 1, 1, 1, 1, 1, 3],
+  [3, 0, 0, 0, 0, 0, 1, 3],
+  [3, 0, 0, 0, 0, 0, 0, 3],
+  [3, 0, 0, 0, 0, 0, 0, 3],
+  [3, 0, 0, 0, 0, 0, 0, 3],
+  [3, 0, 0, 0, 0, 0, 0, 3],
+  [3, 2, 0, 0, 0, 0, 0, 3],
   [3, 3, 3, 3, 3, 3, 3, 3],
 ];
-let table = document.createElement('table');
 var player_figures = ['X', 'O'];
-var player = player_figures[0];
+var active_player = player_figures[0];
 var turn_count = new Counter();
-
-createTable(table, tableArr, parent_table, turn_count, player);
--- server.js
+++ server.js
@@ -1,5 +1,47 @@
-const { Client } = require('pg'); 
- 
+const express = require('express');
+const session = require('express-session');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = 3000;
+
+io.on('connection', socket => {
+  console.log(socket.id);
+});
+
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  const gameStarted = req.session.gameStarted || false;
+  res.render('homepage', { gameStarted });
+});
+
+app.get('/player1', (req, res) => {
+  req.session.gameStarted = true;
+  console.log('Player 1 joined');
+  res.render('virus');
+});
+
+app.get('/player2', (req, res) => {
+  const gameStarted = req.session.gameStarted || false;
+  if (gameStarted) {
+    console.log('Player 2 joined');
+  }
+  res.render('virus');
+});
+
+http.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+/*
 // Подключение к PostgreSQL 
 const client = new Client({ 
   user: 'postgres', 
@@ -18,3 +60,4 @@ client.query('SELECT * FROM players', (err, res) => {
   
   client.end(); 
 });
+*/
