bc5b52faea3d88a58e9a73bf3aa43c520343773e
Manymoera <soloninko80@gmail.com>
2023-12-15 17:35:44 +0300

    added comments

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const io = require('socket.io')(http);
 const port = 3000;
 const today_date = new Date();
 const {Client} = require('pg');
+var game_is_started;
 
 // Подключение к PostgreSQL 
 const client = new Client({ 
@@ -18,6 +19,8 @@ const client = new Client({
  
 client.connect();
 
+//Подключение к сокету происходит в virus.js в
+//socket.on('connect')
 io.on('connection', (socket) => {
   let startTime;
   let endTime;
@@ -38,7 +41,11 @@ io.on('connection', (socket) => {
       year: 'numeric'
     });
   });
+  //Принимает с клиента параметры, указанные в скобках
   socket.on('send-table', (tableArr, flag, active_player) => {
+    //Здесь emit передаёт объект с параметрами в фиг. скобках
+    //Внутри фигурных скобок создаётся объект с ключами, именами которых будут
+    //их переменные
     socket.broadcast.emit('receive-table', {tableArr, flag, active_player});
     console.log(tableArr);
     console.log("--------------------");
@@ -63,28 +70,45 @@ io.on('connection', (socket) => {
   });
 });
 
+//Функция создания сессии
 app.use(session({
+  //строка, которой подписывается сохраняемый в cookie идентификатор сессии
   secret: '12345',
+  /*булевое значение, указывает, нужно ли пересохранять сессию в хранилище, 
+  если она не изменилась*/
   resave: false,
-  saveUninitialized: true,
+  //булевое значение, если true, то в хранилище будут попадать пустые сессии;
+  saveUninitialized: true, 
 }));
 
+//Указываем, что надо использовать шаблонизатор ejs
 app.set('view engine', 'ejs');
+//Обслуживает статичные файлы в папке 'public'
 app.use(express.static('public'));
 
+/* 
+Проверяет началась ли игра и, если нет, то рендерит страницу с start game. 
+В обратном случае рендерит с join game. 
+Здесь в шаблонизатор передается параметр game_is_started
+*/
+
+
+
 app.get('/', (req, res) => {
-  const game_is_started = req.session.game_is_started;
   res.render('homepage', { game_is_started });
 });
 
+//Отличие app.post() от app.get():
+//get - собирает данные с сервера и не изменяет его состояние
+//post - отправляет данные на сервер и может изменять его состояние
+
 app.get('/player1', (req, res) => {
-  req.session.game_is_started = true;
+  game_is_started = true;
   console.log('Player 1 joined');
   res.render('virus');
 });
 
 app.get('/player2', (req, res) => {
-  const game_is_started = req.session.game_is_started;
   if (game_is_started) {
     console.log('Player 2 joined');
   }
